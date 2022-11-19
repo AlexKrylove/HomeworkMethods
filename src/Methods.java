e8d48ecb40b810a65c16de325f25dde419138fbb
AlexKrylov <krylofffff93@gmail.com>
2022-11-19 14:10:39 +0300

    Task1

---
+++ src/Methods.java
@@ -0,0 +1,18 @@
+public class Methods {
+
+    public static int getLeapYear(int leapYear) {
+        int year = 2022;
+        if (year % 4 == 0 && year % 400 != 0 || year % 400 == 0) {
+            System.out.println(year + " год високосный");
+        } else {
+            System.out.println(year + " год не високосный");
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        int year = 0;
+        getLeapYear(year);
+    }
+}
+
